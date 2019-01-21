100066dab3d57dd71e8c0d58221cdd6b05ff4cf9
Faisal <faisalamin1999@googlemail.com>
2019-01-21 22:11:25 +0000

    added css and js for login page

--- login.js
+++ login.js
@@ -2,15 +2,19 @@ $(document).ready(function(){
   $('#login-link').click(function(e) {
     $("#login-form").delay(100).fadeIn(100);
     $("#register-form").fadeOut(100);
-    $('#register-link').removeClass('active');
-    $(this).addClass('active');
+    $("#login-link").css("color", "#0E6655");
+    $("#login-link").css("font-size", "18px");
+    $("#register-link").css("color", "#34495E");
+    $("#register-link").css("font-size", "13px");
     e.preventDefault();
    })
   $("#register-link").click(function(e){
     $("#register-form").delay(100).fadeIn(100);
     $("#login-form").fadeOut(100);
-    $('#login-link').removeClass('active');
-    $(this).addClass('active');
+    $("#register-link").css("color", "#0E6655");
+    $("#register-link").css("font-size", "18px");
+    $("#login-link").css("color", "#34495E");
+    $("#login-link").css("font-size", "13px");
     e.preventDefault();
   })
 })
